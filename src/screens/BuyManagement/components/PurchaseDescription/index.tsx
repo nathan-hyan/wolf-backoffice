620266b246df3c48fe2fa5d6d4cd9d4569479ef6
Hy-An <nathan.hyan2048@gmail.com>
2021-10-04 13:12:35 -0300

    Added testing

--- src/screens/BuyManagement/components/PurchaseDescription/index.tsx
+++ src/screens/BuyManagement/components/PurchaseDescription/index.tsx
@@ -10,18 +10,18 @@ function PurchaseDescription({
       <Row>
         <Col>
           <h1 className="lead fs-4">Compras recibidas este mes:</h1>
-          <p className="fw-bold fs-3">{monthPurchases || 0}</p>
+          <p data-testId="monthPurchases" className="fw-bold fs-3">{monthPurchases || 0}</p>
           <h2 className="lead fs-4">En total:</h2>
-          <p className="fw-bold fs-3">{totalPurchases || 0}</p>
+          <p data-testId="totalPurchases" className="fw-bold fs-3">{totalPurchases || 0}</p>
         </Col>
         <Col>
           <h1 className="lead fs-4">Total suma de productos:</h1>
-          <p className="fw-bold fs-3">
+          <p data-testId="monthAmount" className="fw-bold fs-3">
             $
             {monthAmount || 0}
           </p>
           <h2 className="lead fs-4">En total:</h2>
-          <p className="fw-bold fs-3">
+          <p data-testId="totalAmount" className="fw-bold fs-3">
             $
             {totalAmount || 0}
           </p>
